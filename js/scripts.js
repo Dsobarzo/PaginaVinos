91672564aec138b038cff431d1f60043751b482f
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-01-07 23:18:09 -0300

    se agrega funcion click

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+let menu = document.querySelector('#menu');
+let toggleicon = document.querySelector('#toggle-icon');
+
+toggleicon.addEventListener("click", function(){
+    alert("Funciona");
+});
